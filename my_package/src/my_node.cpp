a727f13ef0d3e613c52b890de5a4496e58d09887
pymc20 <pymc20@naver.com>
2024-10-15 22:05:56 +0900

    add srvcli

--- my_package/src/my_node.cpp
+++ my_package/src/my_node.cpp
@@ -8,3 +8,5 @@ int main(int argc, char ** argv)
   printf("hello world my_package package\n");
   return 0;
 }
+
+docker run -p 6080:80 --security-opt seccomp=unconfined --shm-size=512m tiryoh/ros2-desktop-vnc:jazzy
